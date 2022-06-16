72286eed61cbde59f2e56f370c0b557e506657e9
Alessandro Paci <paci.alessandro88@gmail.com>
2022-06-16 10:42:27 +0200

    suddivisione app-body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { BodyComponent } from './components/body/body.component';
 import { ReviewComponent } from './components/review/review.component';
+import { OfferComponent } from './components/offer/offer.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ReviewComponent } from './components/review/review.component';
     HeaderComponent,
     FooterComponent,
     BodyComponent,
-    ReviewComponent
+    ReviewComponent,
+    OfferComponent
   ],
   imports: [
     BrowserModule,
