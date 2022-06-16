98c5e8d2bde227d856cc355b7f7f5f9dc7e07504
Alessandro Paci <paci.alessandro88@gmail.com>
2022-06-16 10:25:40 +0200

    Inizio creazione componenti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LogosComponent } from './components/logos/logos.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BodyComponent } from './components/body/body.component';
+import { ReviewComponent } from './components/review/review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogosComponent,
+    HeaderComponent,
+    FooterComponent,
+    BodyComponent,
+    ReviewComponent
   ],
   imports: [
     BrowserModule,
